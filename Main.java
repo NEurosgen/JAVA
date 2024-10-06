7e3757330030d1386680f1b088d278cd61ad0c1f
EugenDid <108171626+NEurosgen@users.noreply.github.com>
2024-10-06 22:19:12 +0300

    prac_3_6

--- Main.java
+++ Main.java
@@ -1,15 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int a = 10;
-        double b = 20.5;
-        boolean flag = true;
-        String name = "Alice";
-        String result1 = name + " is " + a + " years old.";
-        String result2 = "Sum of " + a + " and " + b + " is " + (a + b) + ".";
-        String result3 = "The flag is " + flag + ".";
-        System.out.println(result1); 
-        System.out.println(result2);  
-        System.out.println(result3);  
-      
+        double b = 5.5;
+
+        double result = a + b;  // int преобразуется в double
+        System.out.println("Result: " + result);  // 15.5
     }
 }
