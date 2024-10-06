bcac727822c4748579536f0af52685aa867d98d6
EugenDid <108171626+NEurosgen@users.noreply.github.com>
2024-10-06 22:16:18 +0300

    prac_3_5

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\u0416\u0435\u043D\u044F");
+        int a = 10;
+        double b = 20.5;
+        boolean flag = true;
+        String name = "Alice";
+        String result1 = name + " is " + a + " years old.";
+        String result2 = "Sum of " + a + " and " + b + " is " + (a + b) + ".";
+        String result3 = "The flag is " + flag + ".";
+        System.out.println(result1); 
+        System.out.println(result2);  
+        System.out.println(result3);  
+      
     }
 }
