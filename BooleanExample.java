61f1f8b9652b76960a82529e5831c07b6a3f33a7
EugenDid <108171626+NEurosgen@users.noreply.github.com>
2024-11-24 09:41:50 +0300

    Add files via upload

---
+++ BooleanExample.java
@@ -0,0 +1,27 @@
+
+    public class BooleanExample {
+        public static void main(String[] args) {
+            
+            Boolean b1 = new Boolean(true);
+            Boolean b2 = new Boolean("true");
+    
+     
+            Boolean b3 = Boolean.valueOf(false);
+            Boolean b4 = Boolean.valueOf("false");
+    
+        
+            Boolean b5 = true;
+    
+   
+            Boolean b6 = Boolean.parseBoolean("true");
+    
+        
+            System.out.println(b1); 
+            System.out.println(b2);
+            System.out.println(b3); 
+            System.out.println(b4); 
+            System.out.println(b5); 
+            System.out.println(b6); 
+        }
+    }
+    
