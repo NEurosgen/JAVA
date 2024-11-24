54ea2dc53359a2f6903e887c29a1709b2beae20c
EugenDid <108171626+NEurosgen@users.noreply.github.com>
2024-11-24 12:40:46 +0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,17 @@
+public class A {
+    public final int a = 10;  // Инициализация при объявлении
+}
+public class A {
+    public final int a;
+
+    public A(int a) {
+        this.a = a;  
+    }
+}
+public class A {
+    public final int a;
+
+    {
+        a = 10;  
+    }
+}
