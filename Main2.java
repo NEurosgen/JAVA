533f0b78939678e33a22c5e78870d526842749fa
EugenDid <108171626+NEurosgen@users.noreply.github.com>
2024-12-03 18:15:56 +0300

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,20 @@
+final class FinalClass {
+    public void display() {
+        System.out.println("This is a method in a final class.");
+    }
+}
+
+// Попытка наследоваться от final-класса вызовет ошибку компиляции.
+class SubClass extends FinalClass {
+    @Override
+    public void display() {
+        System.out.println("Attempting to override method from FinalClass.");
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        SubClass obj = new SubClass();
+        obj.display();
+    }
+}
